68516cc81d91537900a1c78e0bad2db7ab697155
AlvinSteven <53334961+AlvinSteven@users.noreply.github.com>
2020-01-06 16:39:38 +0800

    secount try

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="Login">
+    <el-card>
         <!--<pre v-text="$attrs"/>-->
       用户名: <input type="text" v-model="loginForm.username" placeholder="请输入用户名..."/>
       <br><br>
       密&nbsp;&nbsp;&nbsp;&nbsp;码: <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
       <br><br>
       <button v-on:click="login">登录</button>
-    </div>
+    </el-card>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
@@ -12,9 +14,12 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册,之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 
+Vue.use(ElementUI);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
