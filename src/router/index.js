4d7d92c8578245ef852b131a4bef2c60354c75d9
AlvinSteven <53334961+AlvinSteven@users.noreply.github.com>
2020-04-16 11:07:08 +0800

    secount try

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Appindex from '@/components/home/Appindex/Appindex'
 import Login from '@/components/Login/Login'
+import Home from '@/components/Home'
+import LibraryIndex from '@/components/library/LibraryIndex/LibraryIndex'
 
 Vue.use(Router)
 
@@ -14,14 +16,38 @@ export default new Router({
       name: 'Login',
       component: Login
     },
-    {
+    /*{
       path: '/welcome',
       name: 'Appindex',
       component: Appindex,
       meta: {
         requireAuth: true
       }
+    }*/
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          name: 'Appindex',
+          component: Appindex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
     }
-
   ]
 })
