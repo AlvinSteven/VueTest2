aa4c2b7e68f1c96481bf5ee44e85307f9780088b
AlvinSteven <53334961+AlvinSteven@users.noreply.github.com>
2020-07-13 10:13:19 +0800

    ohh

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 // 设置反向代理,前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 
-// 切换本地/服务器 f
+// 切换本地/服务器 g
 // axios.defaults.baseURL = 'http://localhost:8443/api'
 axios.defaults.baseURL = 'http://fancywonderland.com:8443/api'
 // 全局注册,之后可在其他组件中通过 this.$axios 发送数据
